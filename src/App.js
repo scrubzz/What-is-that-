b9596b91325b4502eebc09805c743480dd80dae0
Princeton Baker <princeton.bake10@gmail.com>
2020-06-29 14:42:18 -0400

    id works needs api call

--- src/App.js
+++ src/App.js
@@ -3,72 +3,88 @@ import * as mobilenet from '@tensorflow-models/mobilenet';
 
 import './App.css';
 
-const stateMachine = {
-  initial: 'initial',
+const machine = {
+  initial: "initial",
   states: {
-    initial: { on: { next: 'loadingModel' } },
-    loadingModel: { on: { next: 'awaitingUpload' } },
-    awaitingUpload: { on: { next: 'ready' } },
-    ready: { on: { next: 'classifying' }, showImage: true },
-    classifying: { on: { next: 'complete' } },
-    complete: { on: { next: 'awaitingUpload' }, showImage: true },
+    initial: { on: { next: "loadingModel" } },
+    loadingModel: { on: { next: "modelReady" } },
+    modelReady: { on: { next: "imageReady" } },
+    imageReady: { on: { next: "identifying" }, showImage: true },
+    identifying: { on: { next: "complete" } },
+    complete: { on: { next: "modelReady" }, showImage: true, showResults: true }
   }
-}
-
-const reducer = (currentState, event) => stateMachine.states[currentState].on[event] || stateMachine.initial;
-
+};
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, stateMachine.initial);
+  const [results, setResults] = useState([]);
+  const [imageURL, setImageURL] = useState(null);
   const [model, setModel] = useState(null);
-  const [imageUrl, setImageUrl] = useState(null);
-  const [results, setResults] = useState([])
-  const inputRef = useRef();
   const imageRef = useRef();
+  const inputRef = useRef();
 
-  const next = () => dispatch('next');
+  const reducer = (state, event) =>
+    machine.states[state].on[event] || machine.initial;
+
+  const [appState, dispatch] = useReducer(reducer, machine.initial);
+  const next = () => dispatch("next");
 
   const loadModel = async () => {
     next();
-    const mobilenetModel = await mobilenet.load();
-    setModel(mobilenetModel);
+    const model = await mobilenet.load();
+    setModel(model);
     next();
-  }
+  };
 
   const identify = async () => {
-    next()
-    const results = await model.classify(imageRef.current)
-    next()
-  }
+    next();
+    const results = await model.classify(imageRef.current);
+    setResults(results);
+    next();
+  };
 
+  const reset = async () => {
+    setResults([]);
+    next();
+  };
 
-  const handleUpload = e => {
-    const { files } = e.target;
+  const upload = () => inputRef.current.click();
+
+  const handleUpload = event => {
+    const { files } = event.target;
     if (files.length > 0) {
-      const url = URL.createObjectURL(files[0]);
-      setImageUrl(url);
+      const url = URL.createObjectURL(event.target.files[0]);
+      setImageURL(url);
       next();
     }
-  }
+  };
 
-  const buttonProps = {
-    initial: { text: 'Load Model', action: loadModel },
-    loadingModel: { text: 'Loading Model' },
-    awaitingUpload: { text: 'Upload Photo', action: () => inputRef.current.click },
-    ready: { text: 'Identify', action: identify },
-    classifying: { text: 'Identifying', action: () => { } },
-    complete: { text: 'Reset', action: () => { } },
-  }
-
-  const { showImage } = stateMachine.states[state]
+  const actionButton = {
+    initial: { action: loadModel, text: "Load Model" },
+    loadingModel: { text: "Loading Model..." },
+    modelReady: { action: upload, text: "Upload Image" },
+    imageReady: { action: identify, text: "Identify Breed" },
+    identifying: { text: "Identifying..." },
+    complete: { action: reset, text: "Reset" }
+  };
+  const { showImage, showResults } = machine.states[appState]
 
 
   return (
     <div>
-      {showImage && <img alt="upload-preview" src={imageUrl} ref={imageRef} />}
-      <img src={imageUrl} alt="upload preview" ref={imageRef} />
-      <input type="file" accept="image/*" capture="camera" ref={inputRef} onChange={handleUpload} />
-      <button onClick={buttonProps[state].action}>{buttonProps[state].text}</button>
+      {showImage && <img src={imageURL} alt="upload-preview" ref={imageRef} />}
+      <input type="file" accept="image/*" capture="camera" onChange={handleUpload} ref={inputRef} />
+      {showResults && (
+        <ul>
+          {results.map(({ className, probability }) => (
+            <li key={className}>{`${className}: %${(probability * 100).toFixed(2
+            )}`}
+            </li>
+          ))}
+        </ul>
+      )}
+      <button onClick={actionButton[appState].action || (() => { })}>
+        {actionButton[appState].text}
+      </button>
     </div>
   );
 }
